8fabe8455990c1c4c3208955974bd918dc100f2c
itwasonlyabug <meikyo@protonmail.com>
2025-02-05 12:21:17 +0200

    extract autoshutdown script from config

--- configuration.nix
+++ configuration.nix
@@ -2,89 +2,15 @@
 
 let
   s3_save_bucket = "satisfactoryserversaves";
+  
+  autoshutdown_script_url = "https://raw.githubusercontent.com/itwasonlyabug/satisfactory/refs/heads/main/autoshutdown.sh";
+  autoshutdown_webhook_token = "$(${pkgs.awscli2}/bin/aws ssm get-parameter --name 'discord' --query 'Parameter.Value' --output text)";
 
+  satisfactory_home_dir = "/home/satisfactory";
   satisfactory_dir = "/var/lib/SatisfactoryDedicatedServer";
   satisfactory_install = "${pkgs.steamPackages.steamcmd}/bin/steamcmd +login anonymous +force_install_dir ${satisfactory_dir} +app_update 1690800 validate +quit";
-  satisfactory_save_dir = "/home/satisfactory/.config/Epic/FactoryGame/Saved/SaveGames";
-  satisfactory_dotsteam_dir = "/home/satisfactory/.steam/steam";
-  script_dir = "/home/satisfactory/scripts";
-  discord_webhook_url = "";
-
-  autoshutdown = pkgs.writeShellScriptBin "autoshutdown" ''
-  #!/bin/sh
-  BIN_PATH="/run/current-system/sw/bin"
-  webhook() {
-    URL=$discord_webhook_url
-    DATA=$($BIN_PATH/cat << EOF
-    {
-      "username": "Ficsit Corp",
-      "embeds": [{
-        "title": "AWS Instance Status",
-        "description": "Instance is shutting down...",
-        "color": "45973"
-      }]
-    }
-EOF
-  )
-  $BIN_PATH/curl -H "Content-Type: application/json" -X POST -d "$DATA" $URL
-  }
-  sumOfArrayElements() {
-    BYTES=("$@")
-    # returns the sum of all elements in an array
-    SUM=$(IFS=+; echo "$((''${BYTES[*]}))")
-    echo "$SUM"
-  }
-  
-  checkForTraffic() {
-    PORT_TO_CHECK=$1
-    NUMBER_OF_CHECKS=$2
-    
-    for ((connections=0; connections<$NUMBER_OF_CHECKS; connections++)); do
-        CHECK_CONNECTION_BYTES=$($BIN_PATH/ss -luna "( dport = :''${PORT_TO_CHECK} or sport = :''${PORT_TO_CHECK} )" | $BIN_PATH/awk -F ' ' '{s+=$2} END {print s}')
-        CONNECTION_BYTES+=($CHECK_CONNECTION_BYTES)
-    done
-    
-    CONNECTION_BYTES_SUM=$(sumOfArrayElements "''${CONNECTION_BYTES[@]}")
-    echo "$CONNECTION_BYTES_SUM"
-  }
-  
-  shutdownSequence() {
-    echo "No activity detected. Shutting down."
-    systemctl stop satisfactory.service
-    systemctl start satisfactory-backup.service
-    webhook
-    sleep 10
-    shutdown -h now
-  }
-  
-  main(){
-    IDLE_COUNTER=0
-    TOTAL_IDLE_SECONDS=300
-  
-    GAME_PORT=7777
-    CHECKS=5
-  
-    while [ $IDLE_COUNTER -lt $TOTAL_IDLE_SECONDS ]; do
-        ACTIVE_CONNECTIONS=$(checkForTraffic $GAME_PORT $CHECKS)
-  
-        if [ "$ACTIVE_CONNECTIONS" -eq 0 ]; then
-            IDLE_COUNTER=$((''$IDLE_COUNTER+1))
-            echo "No connection detected."
-        
-        elif [ "$ACTIVE_CONNECTIONS" -gt 0 ]; then
-  	  echo "Connection detected."
-            IDLE_COUNTER=0
-        fi
-        sleep 1
-  
-    done
-  
-    shutdownSequence
-  }
-  
-  main 
-  '';
- 
+  satisfactory_save_dir = "${satisfactory_home_dir}/.config/Epic/FactoryGame/Saved/SaveGames";
+  satisfactory_dotsteam_dir = "${satisfactory_home_dir}/.steam/steam";
 in
 
 {
@@ -116,7 +42,7 @@ imports = [
 
   users.users.satisfactory = {
     isNormalUser  = true;
-    home  = "/home/satisfactory";
+    home  = "${satisfactory_home_dir}";
     extraGroups = ["wheel"];
   };
 
@@ -125,16 +51,6 @@ imports = [
   '';
 
   systemd.services = {
-    #pull-scripts = {
-    #  description = "Check if the scripts are present or clone the repo";
-    #  enable = true;
-    #  serviceConfig = {
-    #    Type = "oneshot";
-    #    ExecStart = "${pkgs.git}" clone https://github.com/itwasonlyabug/satisfactory.git "${script_dir}/";
-    #  };
-    #  wantedBy = [ "multi-user.target" ];
-    #};
-
     satisfactory-init = {  
       description = "Prep SteamCMD and install Satisfactory";
       enable = true;
@@ -199,12 +115,26 @@ imports = [
       after = [ "satisfactory.service" ];
     };
     
+    autoshutdown-init = {
+      description = "Pull autoshutdown script";
+      wants = [ "network-online.target"];
+      enable = true;
+      script = ''
+        ${pkgs.wget}/bin/wget "${autoshutdown_script_url}" -O "${satisfactory_home_dir}/autoshutdown.sh"
+        chmod +x "${satisfactory_home_dir}/autoshutdown.sh"
+        '';
+      serviceConfig = {
+        Type = "oneshot";
+      };
+      wantedBy = [ "multi-user.target" ];
+    };
+    
     autoshutdown = {
       description = "Shutdown after 5 minutes of inactivity";
       wants = [ "network-online.target"];
       enable = true;
-      script = "${autoshutdown}/bin/autoshutdown";
-      after = [ "satisfactory.service" ];
+      script = "${satisfactory_home_dir}/autoshutdown.sh ${autoshutdown_webhook_token}";
+      after = [ "satisfactory.service" "autoshutdown-init.service" ];
       wantedBy = [ "multi-user.target" ];
     };
   };
@@ -216,7 +146,7 @@ imports = [
     timerConfig = {
       OnCalendar = "*:0/5";
       Unit = "satisfactory-backup.service";
-    };
+   };
   };
 
   # Copy the NixOS configuration file and link it from the resulting system
@@ -231,4 +161,5 @@ imports = [
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
+
 }
